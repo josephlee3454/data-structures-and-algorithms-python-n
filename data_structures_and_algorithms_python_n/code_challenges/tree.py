d138ead967f4d6c119404d97f5c39e703ecc66cd
josephlee3454 <joseph.lee3454@gmail.com>
2020-10-06 16:39:46 -0700

    added mor to tree

--- data_structures_and_algorithms_python_n/code_challenges/tree.py
+++ data_structures_and_algorithms_python_n/code_challenges/tree.py
@@ -1,18 +1,50 @@
-class Node(object):
-  def __init__(self, value):
-    self.value = value
+class Node:
+  def __init__(self, val=None):
+    self.val = val
     self.left  = None 
     self.right = None 
 
-class Binary_Tree(object):
-  def __init__(self, root):
-    self.root = Node(root)
+class BST:
+  def __init__(self):
+    self.root = None 
 # 1-2-5-6-4-7-8-
 #         1
 #       /    \
 #      2       3
 #    /   \   /   \
 #   4     5 6      7 the intial set up
+def insert(self,val):
+  if self.root == None:
+    self.root=node(val)
+  else: 
+    self._insert(val,self.root)
+
+def _insert(self, val, curr_node):
+  if val < curr_node.val:
+    if curr_node.left == None:
+      cur_node .left = Node(val)
+    else:
+      self._insert(val,curr_node.left)
+  elif val > cur_node.val:
+    if cur_node.right==None:
+      cur_node.right=Node(val)
+    else:
+      self._insert(val,cur_node.right)
+  else:
+    print("the node is already in tree")
+
+
+  def print_bst(self):
+    if self.root != None:
+      self._print_tree(self.root)
+  
+  def _print_bst(self,curr_node):
+    if curr_node != None:
+      self._print_bst (curr_node.left)
+      print(str(curr_node.value))
+      self._print_bst (curr_node.right)
+
+    
   def print_tree(self, traversal_type):
     if traversal_type == "preorder":
       return self.preorder_print(tree.root, "")
@@ -43,48 +75,18 @@ class Binary_Tree(object):
       traversal += (str(start.value) + "-")
     return traversal
 
-class Bst:
-  def insert(root, node):
-    if root is None:
-      root = node
-    fizz_buzz_replace()
-    else:
-      if root.val < node.val:
-        if root.right is None:
-          root.right = node
-        else:
-          insert(root.right, Node)
-      else: 
-        if root.left is None:
-          root.left = none 
-      else: 
-        if root.left is None:
-          root.left = node 
-        else:
-          insert(root.left, node)
+  def fill_tree(tree,num_elems=100,Max_int=1000):
+    from random import randint 
+    for _ in range(nums_elems):
+      curr_elem = randint(0,max_int)
+      tree.insert(curr_elem)
+    return tree
 
-  def fizz_buzz_replace(self):
-    if node(val) % 3 == 0:
-      node(val) = 'fizz'
-    elif node(val)%5 == 0:
-	    node(val) = 'buzz'
-    elif node(val) % 3 == 0 and node(val)%5:
-      node(val) = ‘fizz-buzz’
-  return node val 
+
+tree = BST()
+tree = fill_tree(tree)
+tree.print_tree()
 
 
 
 
-tree = Binary_Tree(1)
-tree.root.left = Node(2)
-tree.root.right = Node(3)
-tree.root.left.left = Node(4)
-tree.root.left.right = Node(5)
-tree.root.right.left = Node(6)
-tree.root.right.right = Node(7)
-tree.root.right.right.right = Node(8)
-
-
-
-
-print(tree.print_tree("postorder"))
